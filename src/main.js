e2138a1b14d86f56d6f09f4c84236abf45fd12aa
Matheus <91149331+matheusmmeneses@users.noreply.github.com>
2024-01-13 03:29:41 -0300

    base design

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+//import Toast from 'vue-toastification';
+//import 'vue-toastification/dist/index.css';
+import './assets/style.css';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-const app = createApp(App)
-
-app.mount('#app')
+const app = createApp(App);
+//app.use(Toast);
+app.mount('#app');
